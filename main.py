19816ae5a0e4f4ea786e85f3df881c44dc04fe82
zhum0002 <zhum0002@student.monash.edu>
2022-12-30 02:11:35 +0800

    adding basic functionality

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 import pygame, sys
+from settings import *
+
+
+class Game:
+    def __init__(self):
+
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        # event loop
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # update the screen
+            self.screen.fill('black')
+            pygame.display.update()
+            self.clock.tick(FPS)  # FPS determines how many times the game updates itself
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
